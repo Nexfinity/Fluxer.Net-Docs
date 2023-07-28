442a1604d1b862e2cbc863a833287a0c9e698775
MrCakeSlayer <xan12334@gmail.com>
2023-07-28 04:15:39 -0400

    minor updates

--- assets/scripts/loader.js
+++ assets/scripts/loader.js
@@ -1,10 +1,9 @@
 var inject = `
 <div class="header" id="header">
-<div class="site-title">Squll.Net Tutorial</div>
-<a class="header-button" id="about-header" href="/">About</a>
-<div class="header-button" id="copyright-header">Copyright</div>
+<div class="site-title">Squll.Net Docs</div>
+<a class="header-button" id="about-header" href="/">Home</a>
 <a class="header-button" id="contact-header" href="/contact.html">Contact</a>
-<div class="header-button" id="oss-header">Open Source</div>
+<a class="header-button" id="oss-header" href="https://github.com/Nexfinity/Squll.Net">Open Source</a>
 </div>
 <div class="content">
 <div class="left-sidebar" id="tutorial-list">
