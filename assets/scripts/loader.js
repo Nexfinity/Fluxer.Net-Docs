6d66ef56e088fffb2c0bdff35243bcf21cf3e801
MrCakeSlayer <xan12334@gmail.com>
2024-01-14 17:52:50 -0500

    Squll=>Fluxer

--- assets/scripts/loader.js
+++ assets/scripts/loader.js
@@ -1,9 +1,9 @@
 var inject = `
 <div class="header" id="header">
-<div class="site-title">Squll.Net Docs</div>
+<div class="site-title">Fluxer.Net Docs</div>
 <a class="header-button" id="about-header" href="/">Home</a>
 <a class="header-button" id="contact-header" href="/contact.html">Contact</a>
-<a class="header-button" id="oss-header" href="https://github.com/Nexfinity/Squll.Net">Open Source</a>
+<a class="header-button" id="oss-header" href="https://github.com/Nexfinity/Fluxer.Net">Open Source</a>
 </div>
 <div class="content">
 <div class="left-sidebar" id="tutorial-list">
