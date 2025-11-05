d0fbc2f7098b3146678fe40228886675ae49a22b
MrCakeSlayer <xan12334@gmail.com>
2025-11-05 16:46:40 -0500

    docs overhaul

--- assets/scripts/loader.js
+++ assets/scripts/loader.js
@@ -8,10 +8,18 @@ var inject = `
 <div class="content">
 <div class="left-sidebar" id="tutorial-list">
 <details open>
-    <summary class="tutorial-header" id="tutorial-fb-title">Your First Bot</summary>
-    <div class="tutorial-item" id="tutorial-fb-create">Creating a Bot</div>
-    <div class="tutorial-item" id="tutorial-fb-message">Receiving Messages</div>
-    <div class="tutorial-item" id="tutorial-fb-command">Your First Command</div>
+    <summary class="tutorial-header" id="tutorial-fb-title">Getting Started</summary>
+    <a class="tutorial-item" id="tutorial-fb-create" href="/tutorials/first-bot/1.create.html">Creating a Bot</a>
+    <a class="tutorial-item" id="tutorial-fb-message" href="/tutorials/first-bot/2.messages.html">Receiving Messages</a>
+    <a class="tutorial-item" id="tutorial-fb-command" href="/tutorials/first-bot/3.commands.html">Your First Command</a>
+</details>
+<details open>
+    <summary class="tutorial-header" id="guides-title">Guides</summary>
+    <a class="tutorial-item" id="guide-config" href="/guides/configuration.html">Configuration</a>
+    <a class="tutorial-item" id="guide-api" href="/guides/api-client.html">API Client</a>
+    <a class="tutorial-item" id="guide-gateway" href="/guides/gateway-client.html">Gateway Client</a>
+    <a class="tutorial-item" id="guide-events" href="/guides/events.html">Events Reference</a>
+    <a class="tutorial-item" id="guide-ratelimit" href="/guides/rate-limiting.html">Rate Limiting</a>
 </details>
 </div>
 <div class="true-content">
